858b710312f7b5f59e753a668130292805c12a72
Julia Shcherbachenko <y.scherbachenko@hh.ru>
2023-03-21 13:12:04 +0300

    Homework_3 commit after review

--- src/main/java/lesson2/Task1.java
+++ src/main/java/lesson2/Task1.java
@@ -8,23 +8,23 @@ public class Task1 {
 
   public static void circle() {
     Scanner scanner = new Scanner(System.in);
-
     System.out.println("Введите координаты х: ");
     int x = scanner.nextInt();
-
     System.out.println("Введите координаты y: ");
     int y = scanner.nextInt();
-
     System.out.println("Введите значение радиуса: ");
     int radius = scanner.nextInt();
 
-    double result = (Math.pow(x, 2) + Math.pow(y, 2));
-    double res = Math.pow(radius, 2);
-
-    if (result <= res) {
+    if (pointInTheCircle(x, y, radius)) {
       System.out.println("Точка лежит на окружности или внутри");
     } else {
       System.out.println("Точка вне окружности");
     }
+  }
+
+  private static boolean pointInTheCircle(int x, int y, int radius) {
+    double result = (Math.pow(x, 2) + Math.pow(y, 2));
+    double res = Math.pow(radius, 2);
+    return (result <= res);
   }
 }
